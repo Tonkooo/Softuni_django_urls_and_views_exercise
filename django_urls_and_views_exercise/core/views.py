92708febd9ee663c960ecbcca1e644129de8ccbe
Tonkooo <at.antonio.todorov@gmail.com>
2024-01-19 13:47:44 +0200

    some changes tested

--- django_urls_and_views_exercise/core/views.py
+++ django_urls_and_views_exercise/core/views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request,  *args, **kwargs):
-    return HttpResponse(f"It works with args={args} and kwargs={kwargs}")
+    content = f"It works with:<br/>"
+    f"args={args} and kwargs={kwargs}, <br/>"
+    f"path={request.path}, <br/>"
+    f"method={request.method}<br/>"
+    f"user={request.user}<br/>"
+
+    return HttpResponse(content)
+
